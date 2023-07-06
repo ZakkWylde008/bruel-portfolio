eb218af7a3d443acb9e9e7b7f661ce158843bcf7
RATOMPOARISOA <razafimbelobruel@gmail.com>
2023-07-06 16:09:26 +0300

    prodConfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCefTqIvgYOvovAmCDIKUJVYlmwnkVZuq4",
+    authDomain: "bruelportfolio.firebaseapp.com",
+    projectId: "bruelportfolio",
+    storageBucket: "bruelportfolio.appspot.com",
+    messagingSenderId: "105832684079",
+    appId: "1:105832684079:web:a10515f620438e735644da"
+  }
 };
